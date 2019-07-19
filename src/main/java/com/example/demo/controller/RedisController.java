14cc09b578178ee8899a36698f7fba3a8319da0c
eather_liang <917055347@qq.com>
2019-07-19 09:49:51 +0800

    用户登出监听

--- src/main/java/com/example/demo/controller/RedisController.java
+++ src/main/java/com/example/demo/controller/RedisController.java
@@ -4,10 +4,10 @@ import com.example.demo.utils.MyHttpSessionListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 @RestController
@@ -30,6 +30,17 @@ public class RedisController {
         }else{
             logger.info("用户【"+username+"】登录失败！");
         }
+    }
+        @RequestMapping("/Logout")
+    public void Logout( HttpServletRequest request) {
+        logger.info("用户退出登录开始！");
+        HttpSession session = request.getSession(false);//防止创建Session
+        if(session != null){
+            session.removeAttribute("loginName");
+            session.invalidate();
+        }
+        logger.info("用户退出登录结束！");
+        //return new CommonResult(ResultEnum.SUCCESS.getCode(), "退出成功！");
     }
 
     @RequestMapping("/online")
