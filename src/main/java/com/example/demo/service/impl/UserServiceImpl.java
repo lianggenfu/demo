8950c2c61fac39616354a0ea84bebffddb2a3472
917055347@qq.com <917055347@qq.comm>
2019-08-02 23:33:49 +0800

    springboot热部署

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -6,15 +6,18 @@ import com.example.demo.model.UserSearchCondition;
 import com.example.demo.service.UserService;
 import com.example.demo.utils.ErrorException;
 import com.example.demo.utils.Page;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
 
 @Service
 public class UserServiceImpl implements UserService {
 
-    @Resource
+    @Autowired
     private UserMapper userMapper;
 
     @Override
@@ -39,6 +42,10 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User updateByBo(User user) {
+        User userDb = userMapper.findById(user.getId());
+        if(StringUtils.isEmpty(userDb)){
+            throw new ErrorException("改用户不存在！");
+        }
         userMapper.updateByBo(user);
         return user;
     }
