dfd3c5395480c78816f58a0e0e5cb9e9f05d6a08
eather_liang <917055347@qq.comm>
2019-08-04 23:31:21 +0800

    test

--- src/main/java/com/example/demo/model/bo/AdminBo.java
+++ src/main/java/com/example/demo/model/bo/AdminBo.java
@@ -4,4 +4,14 @@ import com.example.demo.model.BaseMoedel;
 
 public class AdminBo extends BaseMoedel {
     private static final long serialVersionUID = -5436628295081425245L;
+
+    private String nickName;
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
 }
