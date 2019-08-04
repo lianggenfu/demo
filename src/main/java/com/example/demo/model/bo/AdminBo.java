bb2f7511098c6bb31a4bb1d80e5a71bb0273db7e
eather_liang <917055347@qq.comm>
2019-08-04 22:02:09 +0800

    ceshi

---
+++ src/main/java/com/example/demo/model/bo/AdminBo.java
@@ -0,0 +1,7 @@
+package com.example.demo.model.bo;
+
+import com.example.demo.model.BaseMoedel;
+
+public class AdminBo extends BaseMoedel {
+    private static final long serialVersionUID = -5436628295081425245L;
+}
